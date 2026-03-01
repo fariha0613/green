208f7dc641328d7f511abf748b8074f1735e66c6
fariha0613 <farihachow06@gmail.com>
2026-03-02 01:10:41 +0600

    initial

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -107,26 +107,11 @@ class homepage extends StatelessWidget {
               Expanded(
                 child: Column(
                   children: [
-                    Icon(Icons.home, color: Colors.green, size: 32),
+                    Icon(Icons.home, color: Colors.green, size: 30),
                     Text(
                       "Home",
                       style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.green,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-
-              Expanded(
-                child: Column(
-                  children: [
-                    Icon(Icons.search, color: Colors.green, size: 32),
-                    Text(
-                      "Explore",
-                      style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 14,
                           color: Colors.green,
                           fontWeight: FontWeight.bold),
                     ),
@@ -137,16 +122,36 @@ class homepage extends StatelessWidget {
               Expanded(
                 child: GestureDetector(
                   onTap: () {
+                    Navigator.pushNamed(context, "search");
+                  },
+                child: Column(
+                  children: [
+                    Icon(Icons.search, color: Colors.green, size: 30),
+                    Text(
+                      "Explore",
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              ),
+
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
                     Navigator.pushNamed(context, "cartPage");
                   },
                   child: Column(
                     children: [
                       Icon(CupertinoIcons.cart,
-                          color: Colors.green, size: 32),
+                          color: Colors.green, size: 30),
                       Text(
                         "My Cart",
                         style: TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.green,
                             fontWeight: FontWeight.bold),
                       ),
@@ -163,11 +168,11 @@ class homepage extends StatelessWidget {
                   child: Column(
                     children: [
                       Icon(Icons.person,
-                          color: Colors.green, size: 32),
+                          color: Colors.green, size: 30),
                       Text(
                         "Account",
                         style: TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.green,
                             fontWeight: FontWeight.bold),
                       ),
@@ -188,11 +193,11 @@ class homepage extends StatelessWidget {
                   child: Column(
                     children: [
                       Icon(Icons.swap_horiz,
-                          color: Colors.green, size: 32),
+                          color: Colors.green, size: 30),
                       Text(
                         "Switch",
                         style: TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.green,
                             fontWeight: FontWeight.bold),
                       ),
